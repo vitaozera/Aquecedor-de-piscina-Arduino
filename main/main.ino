a115e085f3f80acca4517b75e68f2b8f17275eb2
vitor <vitoralves842@gmail.com>
2016-04-26 22:22:33 -0300

    More basic functions

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,60 @@
+// CONSTANTS
+boolean ON = 1;
+boolean OFF = 0;
+unsigned long filteringCycleStartTime = 0;
+unsigned long filteringCycleStopTime = 21600; // 6 hours
+
+// GLOBAL VARIABLES
+unsigned long time;
+
 void setup() {
-  setupFilteringCycle(); // Configura o ciclo de filtragem automatica
-  LCDDisplay(temp); // Mostra temperatura no LCD
 }
 
 void loop() {
-  
+  // Time since start
+  time = millis();
 }
 
-
-void turnOnMotor(int velocity) {
+// Switches motors ON and OFF
+void switchMotor(int velocity, boolean state) {
   switch(velocity) {
     case 0:
-      switchRelay(0);
+      switchRelay(0, state);
       break;
     case 1:
-      switchRelay(1);
+      switchRelay(1, state);
       break;
     case 2:
-      switchRelay(2);
+      switchRelay(2, state);
       break;
   }
 }
 
-void switchRelay(int index) {
+// Switches compressor ON and OFF
+void switchCompressor(boolean state) {
+    switchRelay(3, state);
+}
+
+// Switches water bomb ON and OFF
+void switchWaterBomb(boolean state) {
+    switchRelay(4, state);
+}
+
+// Switches relays states
+void switchRelay(int index, boolean state) {
 
 }
 
-void setupFilteringCycle() {
+void LCDDisplay(string text) {
   
 }
 
-void LCDDisplay() {
-  
-}
-
+// Checks water temperature
 double checkWaterTemperature() {
 
+}
+
+// Checks water flux
+int waterFlux() {
+  
 }
